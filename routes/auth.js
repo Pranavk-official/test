40e22e499437c7e00fe86e5de2d5c91707b814b9
Pranav K <pranavkcse@gmail.com>
2024-04-20 14:46:29 +0530

    Routes for auth

---
+++ routes/auth.js
@@ -0,0 +1,7 @@
+// Auth Routes
+//  - Login (common for admin)
+//  - Logout (common for admin)
+//  - Register (common for admin, 1 time use for admin)
+//  - Forgot Password
+//  - Reset Password
+//  - Verify Email / OTP
